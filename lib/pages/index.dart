0d3a9574680a2e5af986451590a43292b2bdeff2
bayu <bayuferdiman1@gmail.com>
2023-11-18 14:37:37 +0700

    Set Page & Navbar

---
+++ lib/pages/index.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:pethub/pages/home_page.dart';
+import './forum_page.dart';
+import './map_page.dart';
+import './profil_page.dart';
+
+class Index extends StatefulWidget {
+  const Index({super.key});
+  @override
+  IndexState createState() => IndexState();
+}
+
+class IndexState extends State<Index> {
+  int pageIndex = 0;
+
+  final pages = [
+    const HomePage(),
+    const ForumPage(),
+    const MapPage(),
+    const ProfilPage(),
+  ];
+
+  
+
+  void changePage(int index) {
+    setState(() {
+      pageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[pageIndex],
+      bottomNavigationBar: buildNavBar(context, pageIndex, changePage),
+    );
+  }
+}
+
+Container buildNavBar(BuildContext context, int currentIndex, Function(int) onTap) {
+
+  Color iconColor(int index) {
+    return currentIndex == index ? Colors.black : Colors.grey;
+  }
+  return Container(
+    height: 60,
+    decoration: BoxDecoration(
+      color: Theme.of(context).primaryColor,
+    ),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        IconButton(
+          onPressed: () => onTap(0),
+          icon: Icon(
+            Icons.home_outlined,
+            color: iconColor(0),
+            size: 30,
+          ),
+        ),
+        IconButton(
+          onPressed: () => onTap(1),
+          icon: Icon(
+            Icons.message_outlined,
+            color: iconColor(1),
+            size: 30,
+          ),
+        ),
+        IconButton(
+          onPressed: () => onTap(2),
+          icon: Icon(
+            Icons.location_on_outlined,
+            color: iconColor(2),
+            size: 30,
+          ),
+        ),
+        IconButton(
+          onPressed: () => onTap(3),
+          icon: Icon(
+            Icons.person_outline_rounded,
+            color: iconColor(3),
+            size: 30,
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
---
+++ lib/pages/map_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MapPage extends StatelessWidget {
+  const MapPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xffC4DFCB),
+      child: const Center(
+        child: Text("Map Page"),
+      ),
+    );
+  }
+}
