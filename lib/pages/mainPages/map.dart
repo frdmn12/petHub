f69047cfb77227a212a73283230162f1b0f659e1
Yoga Ageng <yogafriend844@gmail.com>
2023-11-29 03:45:18 +0700

    map doang

--- lib/pages/mainPages/map.dart
+++ lib/pages/mainPages/map.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
   const MapPage({super.key});
 
   @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  static const LatLng _pGooglePlex = LatLng(37.4223, -122.0848);
+  static const LatLng _pApplePark = LatLng(37.3346, -122.0090);
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: const Color(0xffC4DFCB),
-      child: const Center(
-        child: Text("Map Page"),
+    return Scaffold(
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: _pGooglePlex,
+          zoom: 13,
+        ),
+        markers: {
+          Marker(
+              markerId: MarkerId("_currentLocation"),
+              icon: BitmapDescriptor.defaultMarker,
+              position: _pGooglePlex)
+        },
       ),
     );
   }
