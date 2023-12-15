c736457cad4d8593b28b3aa08f5c2e74a6322663
bayu <bayuferdiman1@gmail.com>
2023-12-15 17:22:10 +0700

    Try Repair

--- lib/pages/mainPages/createForum.dart
+++ lib/pages/mainPages/createForum.dart
@@ -62,26 +62,4 @@ class _CreateForumPageState extends State<CreateForumPage> {
   }
 }
 
-// Future<List<String>> getKeywordsFromText(String text) async {
-//   var apiKey = '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';
-//   var endpoint = 'https://api.openai.com/v1/engines/your_engine/keywords';
-
-//   var response = await http.post(
-//     Uri.parse(endpoint),
-//     headers: {
-//       'Content-Type': 'application/json',
-//       'Authorization': 'Bearer $apiKey',
-//     },
-//     body: jsonEncode({'text': text}),
-//   );
-
-//   if (response.statusCode == 200) {
-//     Map<String, dynamic> data = jsonDecode(response.body);
-//     List<String> keywords = List<String>.from(data['keywords']);
-//     return keywords;
-//   } else {
-//     throw Exception('Failed to load keywords');
-//   }
-// }
-
 
