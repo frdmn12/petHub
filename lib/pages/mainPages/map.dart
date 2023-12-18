302eee1af6d80b5d2a8677f80484c15859b77a3f
Yoga Ageng <yogafriend844@gmail.com>
2023-12-18 12:33:25 +0700

    font&search

--- lib/pages/mainPages/map.dart
+++ lib/pages/mainPages/map.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+import 'package:fab_circular_menu/fab_circular_menu.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -9,23 +11,58 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
-  static const LatLng _pGooglePlex = LatLng(37.4223, -122.0848);
-  static const LatLng _pApplePark = LatLng(37.3346, -122.0090);
+  Completer<GoogleMapController> _controller = Completer();
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
 
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final ScreenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      body: GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target: _pGooglePlex,
-          zoom: 13,
+      body: SingleChildScrollView(
+        child: Column(children: [
+          Stack(
+            children: [
+              Container(
+                height: screenHeight,
+                width: ScreenWidth,
+                child: GoogleMap(
+                  mapType: MapType.normal,
+                  initialCameraPosition: _kGooglePlex,
+                  onMapCreated: (GoogleMapController controller) {
+                    _controller.complete(controller);
+                  },
+                ),
+              ),
+            ],
+          )
         ),
-        markers: {
-          Marker(
-              markerId: MarkerId("_currentLocation"),
-              icon: BitmapDescriptor.defaultMarker,
-              position: _pGooglePlex)
-        },
+      ),
+      floatingActionButton: FabCircularMenu(
+        alignment: Alignment.bottomLeft,
+        fabColor: Colors.blue.shade50,
+        fabOpenColor: Colors.red.shade100,
+        ringDiameter: 250.0,
+        ringWidth: 60.0,
+        ringColor: Colors.blue.shade50,
+        fabSize: 60.0,
+        children: [
+          IconButton(
+              onPressed: () {
+                setState(() {});
+              },
+              icon: Icon(Icons.search)),
+          IconButton(
+              onPressed: () {
+                setState(() {});
+              },
+              icon: Icon(Icons.navigation))
+        ],
       ),
     );
   }
