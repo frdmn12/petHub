dda98f0a057741322ce3c7994d3df7d83e60cdaf
ReppoIsYoga <yogafriend844@gmail.com>
2023-12-22 11:10:18 +0700

    search

--- lib/pages/mainPages/map.dart
+++ lib/pages/mainPages/map.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -9,24 +10,80 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
-  static const LatLng _pGooglePlex = LatLng(37.4223, -122.0848);
-  static const LatLng _pApplePark = LatLng(37.3346, -122.0090);
+  Completer<GoogleMapController> _controller = Completer();
+
+  Timer? debounce;
+
+  bool searchToggle = false;
+
+  Set<Marker> _markers = Set<Marker>();
+
+  TextEditingController searchController = TextEditingController();
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
 
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final ScreenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      body: GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target: _pGooglePlex,
-          zoom: 13,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: <Widget>[
+                Container(
+                  height: screenHeight,
+                  width: ScreenWidth,
+                  child: GoogleMap(
+                    mapType: MapType.normal,
+                    markers: _markers,
+                    initialCameraPosition: _kGooglePlex,
+                    onMapCreated: (GoogleMapController controller) {
+                      _controller.complete(controller);
+                    },
+                  ),
+                ),
+                searchToggle
+                    ? Padding(
+                        padding: EdgeInsets.fromLTRB(15.0, 40.0, 15.0, 5.0),
+                        child: Column(children: [
+                          Container(
+                            height: 50.0,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10.0),
+                              color: Colors.white,
+                            ),
+                            child: TextFormField(
+                              controller: searchController,
+                              decoration: InputDecoration(
+                                  contentPadding: EdgeInsets.symmetric(
+                                      horizontal: 20.0, vertical: 15.0),
+                                  border: InputBorder.none,
+                                  hintText: 'Search',
+                                  suffixIcon: IconButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          searchToggle = false;
+                                          searchController.text = '';
+                                        });
+                                      },
+                                      icon: Icon(Icons.close))),
+                            ),
+                          )
+                        ]),
+                      )
+                    : Container()
+              ],
+            )
+          ],
         ),
-        markers: {
-          Marker(
-              markerId: MarkerId("_currentLocation"),
-              icon: BitmapDescriptor.defaultMarker,
-              position: _pGooglePlex)
-        },
       ),
     );
   }
 }
+
